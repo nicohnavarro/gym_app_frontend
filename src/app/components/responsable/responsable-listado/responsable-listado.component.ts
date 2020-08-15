b3e226490ad55860a3adf6656d1a6ac61f6b5b62
nicohnavarro <nicohnavarro@gmail.com>
2020-08-14 23:03:58 -0300

    005-Responsables listado

--- src/app/components/responsable/responsable-listado/responsable-listado.component.ts
+++ src/app/components/responsable/responsable-listado/responsable-listado.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Responsable } from 'src/app/models/responsable';
 
+import { ResponsableService } from 'src/app/services/responsable.service';
 @Component({
   selector: 'app-responsable-listado',
   templateUrl: './responsable-listado.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponsableListadoComponent implements OnInit {
 
-  constructor() { }
+  responsables:Array<Responsable>;
+  displayedColumns: string[] = [
+    'nombre',
+    'apellido',
+    'dni',   
+    'correo',
+    'telefono',
+    'editar',
+    'borrar'
+  ];
+  dataSource;
+
+  constructor(private responsableSvc:ResponsableService) { 
+    this.responsables=new Array<Responsable>();
+  }
 
   ngOnInit(): void {
+    
   }
+
 
 }
--- src/app/pages/listados/listados.component.ts
+++ src/app/pages/listados/listados.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadosComponent implements OnInit {
 
+  alumnoSeleccionado
   constructor() { }
 
   ngOnInit(): void {
