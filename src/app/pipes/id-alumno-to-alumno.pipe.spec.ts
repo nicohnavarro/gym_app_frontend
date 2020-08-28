6f0ebe53131cb2e572bb5dd443b4c75887cd9f6c
nicohnavarro <nicohnavarro@gmail.com>
2020-08-28 03:32:28 -0300

    034-Se agrega pipe de alumno_id

---
+++ src/app/pipes/id-alumno-to-alumno.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { IdAlumnoToAlumnoPipe } from './id-alumno-to-alumno.pipe';
+
+describe('IdAlumnoToAlumnoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new IdAlumnoToAlumnoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/id-alumno-to-alumno.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AlumnoService } from '../services/alumno.service';
+import { IResponseEntity } from '../models/interfaces/responseEntity';
+import { Alumno } from '../models/alumno';
+
+@Pipe({
+  name: 'idAlumnoToAlumno'
+})
+export class IdAlumnoToAlumnoPipe implements PipeTransform {
+
+  constructor(private alumnoSvc: AlumnoService) { }
+  transform(idAlumno: number):Promise<string> {
+    return this.alumnoSvc.getAlumnoById(idAlumno).toPromise().then(resolve =>{
+      return resolve.entity.apellido;
+    })
+  }
+
+}
