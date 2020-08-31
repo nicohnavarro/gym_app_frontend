17921688827526808487b8a9df10cfedf9f846ba
nicohnavarro <nicohnavarro@gmail.com>
2020-08-31 02:02:33 -0300

    041-Se agrega servicio de asistencias

---
+++ src/app/models/asistencia.ts
@@ -0,0 +1,6 @@
+export class Asistencia {
+    fecha:Date;
+    alumno_id:number;
+    categoria_id:number;
+    nivel_id:number;
+}
---
+++ src/app/services/asistencia.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Asistencia } from '../models/asistencia';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AsistenciaService {
+
+  constructor(private http: HttpClient) { }
+
+  public getAllAlumnos():Observable<any>{
+    return this.http.get<any>(environment.API_URL+'asistencia/getAll');
+  }
+  public getAlumnoById(date:Date){
+    return this.http.get<any>(`${environment.API_URL}asistencia/${date}`);
+  }
+
+  public addAsistencia(asistencia:Asistencia){
+    return this.http.post(environment.API_URL+'asistencia/add',asistencia);
+  }
+
+
+}
